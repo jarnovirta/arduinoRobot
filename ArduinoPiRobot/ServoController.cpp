50c0d662ed31fbb85849e9bfab6db2aebc7abee6
jarnovirta <jarnovirta@users.noreply.github.com>
2016-08-14 23:03:20 +0300

    Add files via upload

---
+++ ArduinoPiRobot/ServoController.cpp
@@ -0,0 +1,12 @@
+#include "/Users/Jarno/Arduino/bluetoothOhjausBot/ServoController.h"
+
+ServoController::ServoController() {
+      tiltServo.attach(9);  // attaches the servo on pin 9 to the servo object 
+      panServo.attach(10);
+      setPanAndTilt(90, 90); 
+}
+
+void ServoController::setPanAndTilt(int pan, int tilt) {
+       panServo.write(pan);
+       tiltServo.write(tilt);
+  }
